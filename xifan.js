0fbde0ed22aca8db5877f14b087ca8909b864669
9527willyear <2629563781@qq.com>
2026-07-04 01:09:39 +0800

    Add files via upload

--- xifan.js
+++ xifan.js
@@ -2,8 +2,8 @@ var rule = {
     title: '稀饭动漫',
     host: 'https://anime.xifanacg.com',
     url: '/type/fyclass.html###/type/fyclass/page/fypage.html',
-    searchUrl: '/search/wd/**.html',
-    searchable: 2,
+    searchUrl: '',
+    searchable: 0,
     quickSearch: 0,
     filterable: 0,
     headers: {
@@ -16,16 +16,14 @@ var rule = {
         let html = request(input);
         let m = html.match(/https?:[\\\\/]+[a-zA-Z0-9.%-_\\\\/]+\\.(?:m3u8|mp4|mkv|flv)/gi);
         if (m && m.length > 0) {
-            let url = m[0].replace(/\\\\/g, '');
-            url = url.replace(/\\\\u([0-9a-fA-F]{4})/g, function(_, hex) { return String.fromCharCode(parseInt(hex, 16)); });
-            input = url;
+            input = m[0].replace(/\\\\/g, '').replace(/\\\\u([0-9a-fA-F]{4})/g, function(_, hex) { return String.fromCharCode(parseInt(hex, 16)); });
         }
     `,
     limit: 6,
-    推荐: '.public-list-box;a&&title;img&&data-src;.time-title&&Text;a&&href',
-    一级: '.public-list-box;a&&title;img&&data-src;.time-title&&Text;a&&href',
+    推荐: 'div.public-list-box;a.public-list-exp&&title;img&&data-src;.public-list-prb&&Text;a.public-list-exp&&href',
+    一级: 'div.public-list-box;a.public-list-exp&&title;img&&data-src;.public-list-prb&&Text;a.public-list-exp&&href',
     二级: {
-        title: 'h1&&Text',
+        title: 'h3.slide-info-title&&Text',
         img: 'img.lazy[data-src]&&data-src',
         desc: '.desc&&Text',
         content: '.desc&&Text',
@@ -35,5 +33,5 @@ var rule = {
         list_text: 'body&&Text',
         list_url: 'a&&href'
     },
-    搜索: '.public-list-box;a&&title;img&&data-src;.time-title&&Text;a&&href',
+    搜索: '',
 }
