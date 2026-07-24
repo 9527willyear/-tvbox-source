fb38bb612bfee96071237e1e9a3d0e4e230a604c
9527willyear <2629563781@qq.com>
2026-07-25 00:45:46 +0800

    Add files via upload

--- aikady.js
+++ aikady.js
@@ -10,11 +10,50 @@ var rule = {
     编码: 'utf-8',
     timeout: 10000,
     homeUrl: '/',
-    url: '/search.php?searchtype=5&order=time&tid=fyclass&page=fypage',
+    url: '/search.php?searchtype=5&order=hit&tid=fyclass&page=fypage',
+    filter_url: '/search.php?searchtype=5&order=hit&tid=fyclass&page=fypage&***',
     searchUrl: '/search.php?searchword=***',
     searchable: 1,
     quickSearch: 0,
-    filterable: 0,
+    filterable: 1,
+    filter: {
+        "地区": [
+            { "name": "全部", "value": "" },
+            { "name": "大陆", "value": "area=%E5%A4%A7%E9%99%86" },
+            { "name": "香港", "value": "area=%E9%A6%99%E6%B8%AF" },
+            { "name": "台湾", "value": "area=%E5%8F%B0%E6%B9%BE" },
+            { "name": "日本", "value": "area=%E6%97%A5%E6%9C%AC" },
+            { "name": "韩国", "value": "area=%E9%9F%A9%E5%9B%BD" },
+            { "name": "美国", "value": "area=%E7%BE%8E%E5%9B%BD" },
+            { "name": "英国", "value": "area=%E8%8B%B1%E5%9B%BD" },
+            { "name": "法国", "value": "area=%E6%B3%95%E5%9B%BD" },
+            { "name": "德国", "value": "area=%E5%BE%B7%E5%9B%BD" },
+            { "name": "泰国", "value": "area=%E6%B3%B0%E5%9B%BD" },
+            { "name": "印度", "value": "area=%E5%8D%B0%E5%BA%A6" },
+            { "name": "俄罗斯", "value": "area=%E4%BF%84%E7%BD%97%E6%96%AF" },
+            { "name": "意大利", "value": "area=%E6%84%8F%E5%A4%A7%E5%88%A9" },
+            { "name": "澳大利亚", "value": "area=%E6%BE%B3%E5%A4%A7%E5%88%A9%E4%BA%9A" },
+            { "name": "加拿大", "value": "area=%E5%8A%A0%E6%8B%BF%E5%A4%A7" },
+            { "name": "西班牙", "value": "area=%E8%A5%BF%E7%8F%AD%E7%89%99" },
+            { "name": "土耳其", "value": "area=%E5%9C%9F%E8%80%B3%E5%85%B6" },
+            { "name": "其他", "value": "area=%E5%85%B6%E4%BB%96" }
+        ],
+        "年份": [
+            { "name": "全部", "value": "" },
+            { "name": "2026", "value": "year=2026" },
+            { "name": "2025", "value": "year=2025" },
+            { "name": "2024", "value": "year=2024" },
+            { "name": "2023", "value": "year=2023" },
+            { "name": "2022", "value": "year=2022" },
+            { "name": "2021", "value": "year=2021" },
+            { "name": "2020", "value": "year=2020" },
+            { "name": "2019", "value": "year=2019" },
+            { "name": "2018", "value": "year=2018" },
+            { "name": "2017", "value": "year=2017" },
+            { "name": "2016", "value": "year=2016" },
+            { "name": "2015", "value": "year=2015" }
+        ]
+    },
     limit: 24,
     double: false,
     play_parse: true,
@@ -65,7 +104,6 @@ var rule = {
             VOD.vod_year = pdfh(html, 'a[href*="year="]&&Text') || '';
             VOD.vod_area = pdfh(html, 'a[href*="area="]&&Text') || '';
             VOD.vod_content = pdfh(html, '.stui-content__desc&&Text') || '';
-
             let episodes = pdfa(html, '.stui-content__playlist a[href*="/play/"]').map(a => {
                 let name = pdfh(a, 'a&&Text') || pdfh(a, 'a&&title');
                 let url = pd(a, 'a&&href', input);
