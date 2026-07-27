d2c6ee17ea2222ddb5b3bf911f680d11e29a44f0
9527willyear <2629563781@qq.com>
2026-07-27 21:40:49 +0800

    Add files via upload

--- duboku.js
+++ duboku.js
@@ -92,76 +92,64 @@ var rule = {
             VOD.vod_director = pdfh(html, '.module-info-item:contains(导演)&&Text') || '';
             VOD.vod_content = pdfh(html, '.module-info-introduction&&Text') || '';
 
-            var playFrom = [];
-            var playUrl = [];
-            
-            // 获取视频ID
-            var vid = '';
+            // DEBUG: 收集调试信息
+            var debug = [];
             var vMatch = input.match(/\/v\/(\d+)\.html/);
             var pMatch = input.match(/\/p\/(\d+)-/);
             var playerMatch = html.match(/var player_aaaa=([^;]+);/);
-            if (vMatch) {
-                vid = vMatch[1];
-            } else if (pMatch) {
-                vid = pMatch[1];
-            } else if (playerMatch) {
-                try {
-                    var player = JSON.parse(playerMatch[1]);
-                    vid = player.id || '';
-                } catch (e) {}
+            var vid = '';
+            if (vMatch) vid = vMatch[1];
+            else if (pMatch) vid = pMatch[1];
+            else if (playerMatch) {
+                try { vid = JSON.parse(playerMatch[1]).id || ''; } catch (e) {}
             }
-            
-            if (vid) {
-                // 获取线路名称
-                var sourceNames = [];
-                var tabItems = pdfa(html, '.module-tab-item');
-                for (var t = 0; t < tabItems.length; t++) {
-                    var tab = tabItems[t];
-                    var name = pdfh(tab, 'span&&Text') || pdfh(tab, 'a&&Text') || '';
-                    name = name.replace(/\d+$/, '').trim();
-                    if (name) sourceNames.push(name);
-                }
-                
-                // 获取播放面板
-                var panels = pdfa(html, '.module-list.sort-list.tab-list');
-                if (panels.length === 0) {
-                    panels = pdfa(html, '.module-play-list');
-                }
-                
-                for (var p = 0; p < panels.length; p++) {
-                    var panel = panels[p];
-                    var links = pdfa(panel, '.module-play-list-link');
-                    if (links.length === 0) continue;
-                    
-                    var episodes = [];
-                    var sourceNum = p + 1;
-                    
-                    for (var i = 0; i < links.length; i++) {
-                        var link = links[i];
-                        var epUrl = pdfh(link, 'a&&href');
-                        var epName = pdfh(link, 'span&&Text') || pdfh(link, 'a&&title');
-                        epName = epName.replace(/播放.*第/, '第').replace(/播放.*第/, '第');
-                        
-                        if (epUrl && epUrl.indexOf('/p/') === 0) {
-                            episodes.push(epName + '$' + epUrl);
-                        } else {
-                            episodes.push(epName + '$/p/' + vid + '-' + sourceNum + '-' + (i + 1) + '.html');
-                        }
-                    }
-                    
-                    if (episodes.length > 0) {
-                        var sourceName = sourceNames[p] || ('线路' + sourceNum);
-                        playFrom.push(sourceName);
-                        playUrl.push(episodes.join('#'));
+            debug.push('input=' + input);
+            debug.push('vid=' + vid);
+            debug.push('html_len=' + html.length);
+
+            var tabItems = pdfa(html, '.module-tab-item');
+            var sourceNames = [];
+            for (var t = 0; t < tabItems.length; t++) {
+                var name = pdfh(tabItems[t], 'span&&Text') || pdfh(tabItems[t], 'a&&Text') || '';
+                name = name.replace(/\d+$/, '').trim();
+                if (name) sourceNames.push(name);
+            }
+            debug.push('tabs=' + tabItems.length + ',names=' + sourceNames.join('|'));
+
+            var panels = pdfa(html, '.module-list.sort-list.tab-list');
+            if (panels.length === 0) panels = pdfa(html, '.module-play-list');
+            debug.push('panels=' + panels.length);
+
+            var playFrom = [];
+            var playUrl = [];
+            for (var p = 0; p < panels.length; p++) {
+                var links = pdfa(panels[p], '.module-play-list-link');
+                debug.push('panel' + p + '_links=' + links.length);
+                if (links.length === 0) continue;
+                var episodes = [];
+                for (var i = 0; i < links.length; i++) {
+                    var epUrl = pdfh(links[i], 'a&&href');
+                    var epName = pdfh(links[i], 'span&&Text') || pdfh(links[i], 'a&&title');
+                    epName = epName.replace(/播放.*第/, '第').replace(/播放.*第/, '第');
+                    if (epUrl && epUrl.indexOf('/p/') === 0) {
+                        episodes.push(epName + '$' + epUrl);
+                    } else {
+                        episodes.push(epName + '$/p/' + vid + '-' + (p + 1) + '-' + (i + 1) + '.html');
                     }
                 }
+                if (episodes.length > 0) {
+                    var sourceName = sourceNames[p] || ('线路' + (p + 1));
+                    playFrom.push(sourceName);
+                    playUrl.push(episodes.join('#'));
+                }
             }
-            
+            debug.push('playFrom=' + playFrom.join('|'));
+            VOD.vod_remarks = debug.join(' ; ');
+
             if (playFrom.length === 0) {
                 playFrom.push('独播库');
                 playUrl.push('暂无集数$http://localhost');
             }
-            
             VOD.vod_play_from = playFrom.join('$$$');
             VOD.vod_play_url = playUrl.join('$$$');
         } catch (e) {
